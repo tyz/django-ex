ebab6d313ca272499e18c270200c43722c0a0f8f
Mathijs Maassen <mmaassen@ebay.com>
2017-03-02 11:28:58 +0000

    simple example

--- wsgi.py
+++ wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for project project.
+from flask import Flask
+application = Flask(__name__)
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+@application.route("/")
+def hello():
+    return "Hello World!"
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-
-application = get_wsgi_application()
+if __name__ == "__main__":
+    application.run()
